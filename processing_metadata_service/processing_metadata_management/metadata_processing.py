b7c8e330a7f238bda90788874e8e4517f104ab94
yitzchakdamen <yitzchakdamen@gmail.com>
2025-09-08 09:41:22 +0300

    Successful completion of packaging of the two stages for the dagger - and testing -

--- processing_metadata_service/processing_metadata_management/metadata_processing.py
+++ processing_metadata_service/processing_metadata_management/metadata_processing.py
@@ -1,24 +1,29 @@
 from pathlib import Path
 from datetime import datetime
 from typing import Generator
+from utils.decorators import log_func, safe_execute
 
 
 class FileMetadataProcessing:
     
     def __init__(self, folder_files_path:str) -> None:
-        """ """
         self.folder = Path(folder_files_path)
 
-    def get_files_information_in_folder(self) -> Generator[dict]:
+    @log_func
+    def get_files_information_in_folder(self) -> Generator[dict,None, None]:
         """ Generator for getting information about files within a folder """
-        return iter(self.extracting_all_file_metadata(file) for file in self.folder.iterdir() if file.is_file())
+        for file in self.folder.iterdir(): 
+            if file.is_file():
+                yield self.extracting_all_file_metadata(file)
     
+    @log_func
     def extracting_all_file_metadata(self, file: Path) -> dict:
         """ getting all file information """
         metadata: dict = self.file_metadata(file=file)
         metadata["statistics"] = self.file_statistics(file=file)
         return metadata
-        
+    
+    @log_func
     def file_metadata(self, file: Path) -> dict:
         """ extracting file metadata  """
         if not file.is_file(): raise TypeError("The Path is not a file")
@@ -28,7 +33,8 @@ class FileMetadataProcessing:
             "absolute_path":file.resolve(),
             "metadata": { "stem": file.stem, "suffix": file.suffix}
         }
-        
+    
+    @log_func
     def file_statistics(self, file: Path) -> dict:
         """ extracting file statistics  """
         if not file.is_file(): raise TypeError("The Path is not a file")
@@ -36,6 +42,6 @@ class FileMetadataProcessing:
         return {
             "size": stats.st_size,
             "last_modification_time" : str(datetime.fromtimestamp(stats.st_mtime)),
-            "last_metadata_change_time": str(datetime.fromtimestamp(stats.st_birthtime)),
+            "creation_time": str(datetime.fromtimestamp(stats.st_birthtime if hasattr(stats, "st_birthtime") else stats.st_mtime)),
             "last_access_time": str(datetime.fromtimestamp(stats.st_atime))
         }
